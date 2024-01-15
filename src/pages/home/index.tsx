977ca59839efa71ce6e40a6e9e37f2bff09ef0dd
higorstos <higor.stos@outlook.com>
2024-01-14 23:08:18 -0300

    created home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,47 @@
+import { Link } from 'react-router-dom'
+import { BiSearch } from 'react-icons/bi'
+
+import styles from './home.module.css'
+
 export const Home = () => {
   return(
-    <div>
-      <h1>Página Home</h1>
-    </div>
+    <main className={styles.container}>
+      <form className={styles.form}>
+        <input
+          placeholder="Digite o símbolo da moeda: BTC..."
+        />
+        <button type='submit'>
+          <BiSearch size={30} color="#fff" />
+        </button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th scope='col'>Moeda</th>
+            <th scope='col'>Preço</th>
+            <th scope='col'>Cap. de Mercado</th>
+            <th scope='col'>Volume</th>
+          </tr>
+        </thead>
+        <tbody id='tbody'>
+          <tr className={styles.tr}>
+            <td className={styles.tdLabel} data-label="Moeda">
+              <Link to="/detail/btc" className={styles.link}>
+                <span>Bitcoin</span> | BTC
+              </Link>
+            </td>
+            <td className={styles.tdLabel} data-label="Preço">
+              R$ 40.962
+            </td>
+            <td className={styles.tdLabel} data-label="Cap. de Mercado">
+              R$ 19.293.478
+            </td>
+            <td className={styles.tdLoss} data-label="Volume">
+              <span>-5.3%</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
   )
 }
